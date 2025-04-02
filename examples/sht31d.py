194f692bc7b90f156682407e10dcb5f72c5ba072
Alex Nathanson <alex@alexnathanson.com>
2025-04-02 17:13:22 -0400

    examples

--- examples/sht31d.py
+++ examples/sht31d.py
@@ -11,9 +11,10 @@ sensor.mode = adafruit_sht31d.MODE_SINGLE
 #sensor.mode = adafruit_sht31d.MODE_PERIODIC
 #sensor.frequency = adafruit_sht31d.FREQUENCY_2
 
-print('Humidity: {0}%'.format(sensor.relative_humidity))
-print('Temperature: {0}C'.format(sensor.temperature))
-print('Temperature: {0}C'.format(cToF(sensor.temperature)))
 
 def cToF(c):
 	return c * (9/5) + 32
+	
+print('Humidity: {0}%'.format(sensor.relative_humidity))
+print('Temperature: {0}C'.format(sensor.temperature))
+print('Temperature: {0}C'.format(cToF(sensor.temperature)))
