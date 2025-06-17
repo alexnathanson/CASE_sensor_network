1f20b4bd9f47a915f3d86aff4e297e44d7948d3c
Alex Nathanson <alex@alexnathanson.com>
2025-06-17 17:34:25 -0400

    now with text

--- rpi_zero_sensor/utilities/install_automation.sh
+++ rpi_zero_sensor/utilities/install_automation.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+#cd /home/case/CASE_sensor_network/rpi_zero_sensor/
+
+chmod +x /home/case/CASE_sensor_network/rpi_zero_sensor/sht31d_logger.py
+
+sudo cp /home/case/CASE_sensor_network/rpi_zero_sensor/sh31d_logger.service /etc/systemd/system/sht31d_logger.service
+
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable sht31d_logger.service
+sudo systemctl start sht31d_logger.service
+
+
+chmod +x /home/case/CASE_sensor_network/rpi_zero_sensor/sht31d_dashboard.py
+
+sudo cp sh31d_dashboard.service /etc/systemd/system/sht31d_dashboard.service
+sudo systemctl daemon-reexec sudo systemctl daemon-reload
+sudo systemctl enable sht31d_dashboard.service
+sudo systemctl start sht31d_dashboard.service
