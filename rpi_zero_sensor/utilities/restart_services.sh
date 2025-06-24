f0ce68230512a093a423fefdb9bded50d63afd3f
alexnathanson <alex@alexnathanson.com>
2025-06-23 22:26:45 -0400

    service restarter

--- rpi_zero_sensor/utilities/restart_services.sh
+++ rpi_zero_sensor/utilities/restart_services.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
-# sudo systemctl daemon-reexec
-# sudo systemctl daemon-reload
-# sudo systemctl enable sht31d_logger.service
-# sudo systemctl start sht31d_logger.service
+read -p "Restart temperature and humidity (SHT31-D) logger? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    systemctl daemon-reexec
+    systemctl daemon-reload
+    systemctl enable sht31d_logger.service
+    systemctl start sht31d_logger.service
+    echo "Kasa logger service installed"
+fi
 
-sudo systemctl daemon-reexec
-sudo systemctl daemon-reload
-sudo systemctl enable dashboard.service
-sudo systemctl start dashboard.service
+read -p "Restart Kasa logger? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    systemctl daemon-reexec
+    systemctl daemon-reload
+    systemctl enable kasa_logger.service
+    systemctl start kasa_logger.service
+    echo "Kasa logger service installed"
+fi
+
+read -p "Restart dashboard? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	systemctl daemon-reexec
+	systemctl daemon-reload
+	systemctl enable dashboard.service
+	systemctl start dashboard.service
+fi
+
+read -p "Restart Airtable? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	systemctl daemon-reexec
+	systemctl daemon-reload
+	systemctl enable airtable.service
+	systemctl start airtable.service
+fi
