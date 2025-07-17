d090ec932d0b0bd1f816941427d6afc4e3b93ea9
alexnathanson <alex@alexnathanson.com>
2025-07-17 15:06:48 -0400

    constant temp readings

--- rpi_zero_sensor/sht31d_logger.py
+++ rpi_zero_sensor/sht31d_logger.py
@@ -6,13 +6,17 @@ import os
 import pandas as pd
 import datetime
 import json
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 with open("/home/case/CASE_sensor_network/rpi_zero_sensor/config.json") as f:
     config = json.load(f)
 
 deviceNum = config["sensor"]["number"]
 freq = int(config["sensor"]["frequency_seconds"])
+
+readings = 10
 
 i2c = board.I2C() # this also works
 sensor = adafruit_sht31d.SHT31D(i2c)
@@ -24,40 +28,52 @@ def cToF(c):
 	return c * (9/5) + 32
 
 def main():
+	tempC_list = []
+	humidity_list = []
 
+	startTime = 0
 	while True:
-		tempC = sensor.temperature
-		tempF = cToF(sensor.temperature)
-		humidity = sensor.relative_humidity
 
-		# print(f"\nTemperature: {sensor.temperature:.2f} C")
-		# print(f"Temperature: {cToF(sensor.temperature):.2f} F")
-		# print(f"Humidity: {sensor.relative_humidity:.2f}%")
-		
-		newDF = pd.DataFrame(data={
-			"datetime" : [datetime.datetime.now()],
-			"tempC": tempC,
-			"tempF": tempF,
-			"humidityP": humidity})
-
-		print(newDF)
-
-		# create a new file daily to save data
-		# or append if the file already exists
-		fileName = 'data/sensor' + str(deviceNum) + '_'+str(datetime.date.today())+'.csv'
-
-		try:
-			with open(fileName) as csvfile:
-				df = pd.read_csv(fileName)
-				df = pd.concat([df,newDF], ignore_index = True)
-				#df = df.append(newDF, ignore_index = True)
-				df.to_csv(fileName, sep=',',index=False)
-		except Exception as e:
-			print(e)
-			newDF.to_csv(fileName, sep=',',index=False)
+		#for r in range(readings):
+		tempC_list.append(sensor.temperature)
+		humidity_list.append(sensor.relative_humidity)
 
 		#collect data every 5 minutes
-		time.sleep(freq)
+		if time.time() - freq > startTime:
+			startTime = time.time()
+			logging.debug(startTime)
+
+			tempC = sum(tempC_list)/len(tempC_list)
+			humidity = sum(humidity_list)/len(humidity_list)
+
+			tempC_list = []
+			humidity_list = []
+
+			tempF = cToF(tempC)
+
+			newDF = pd.DataFrame(data={
+				"datetime" : [datetime.datetime.now()],
+				"tempC": tempC,
+				"tempF": tempF,
+				"humidityP": humidity})
+
+			print(newDF)
+
+			# create a new file daily to save data
+			# or append if the file already exists
+			fileName = 'data/sensor' + str(deviceNum) + '_'+str(datetime.date.today())+'.csv'
+
+			try:
+				with open(fileName) as csvfile:
+					df = pd.read_csv(fileName)
+					df = pd.concat([df,newDF], ignore_index = True)
+					#df = df.append(newDF, ignore_index = True)
+					df.to_csv(fileName, sep=',',index=False)
+			except Exception as e:
+				print(e)
+				newDF.to_csv(fileName, sep=',',index=False)
+
+		time.sleep(1)
 
 if __name__ == "__main__":
 	main()
