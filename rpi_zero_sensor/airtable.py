a57d2577840ea1e5ac1ad77e893885d81dba3c36
Alex Nathanson <alex@alexnathanson.com>
2025-09-17 13:56:09 -0400

    undo check data length

--- rpi_zero_sensor/airtable.py
+++ rpi_zero_sensor/airtable.py
@@ -42,7 +42,7 @@ class Airtable():
 
         records = []
         for n in range(len(names)):
-            if (len(data)<n+1) or (data[n]=={}):
+            if data[n]=={}:
                 continue
             try:
                 logging.debug(f'{names[n]}!')
